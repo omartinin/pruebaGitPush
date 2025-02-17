bc64df2d4f7e6e97f96637174ae11a416b676238
omartinin <omartinin@gmail.com>
2025-02-17 14:23:15 +0100

    añadido nombre e instituto a App.java, añadido aviso de modificación en README.md

--- App.java
+++ App.java
@@ -4,6 +4,9 @@ public class App {
     System.out.println("Contenido 2a");
     System.out.println("Contenido 3");
     System.out.println("Contenido 2b");
+    System.out.println("Omar");
+    System.out.println("IES San Andres");
+    System.out.println("Carbajal de la legua");
   }
   
 }
